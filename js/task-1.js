09fc610bcce8eb861010faa290344700844b53dc
Viktoriia Shpak <viktoriia.ienko@gmail.com>
2024-03-12 13:54:30 +0200

    added html documents

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,11 @@
-let items = document.getElementsByClassName('item');
 
-console.log(`Number of categories: ${items.length}`);
-for (let item of items) {
-  let heading = item.getElementsByTagName('h2').item(0).innerHTML;
-  let elements = item.getElementsByTagName('li').length;
-  // let text = heading.innerHTML;
-  console.log(`Category: ${heading} \nElements: ${elements}`);
-}
+
+
+
+
+
+
+
+
+
+
